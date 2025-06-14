64f0fc103688e0bd49f5d38addf7904b57c54225
Paul Duncan <pabs@pablotron.org>
2025-06-14 10:33:03 -0400

    add test/

---
+++ test/test_check.py
@@ -0,0 +1,18 @@
+# import system libraries
+import json, jsonschema, os
+
+# absolute path to base directory
+BASE_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..')
+
+def test_check():
+  """test that KSIs validate against top-level JSON schema"""
+  # read KSIs from "../ksis.json"
+  data = json.load(open(os.path.join(BASE_PATH, 'ksis.json')))
+
+  # read schema from "../schema.json"
+  schema = json.load(open(os.path.join(BASE_PATH, 'schema.json')))
+
+  jsonschema.validate(data, schema)
+
+  # assert that generated json matches expected json
+  assert(1)
---
+++ test/test_parse.py
@@ -0,0 +1,23 @@
+# import system libraries
+import gzip, json, os, sys
+
+# absolute path to base directory
+BASE_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..')
+
+# add base path to search path, import ksihtml library
+sys.path.append(BASE_PATH)
+import ksihtml
+
+def test_ksihtml_parse():
+  """test ksihtml.parse()"""
+  # read expected json from "../ksis.json"
+  exp = open(os.path.join(BASE_PATH, 'ksis.json')).read()
+
+  # read html from "./index.html.gz", parse as ksis
+  ksis = ksihtml.parse(gzip.open(os.path.join(BASE_PATH, 'test', 'index.html.gz')).read())
+
+  # encode ksis as json and append newline
+  got = json.dumps({"version": "20250605", "ksis": ksis}) + "\n"
+
+  # assert that generated json matches expected json
+  assert(got == exp)
